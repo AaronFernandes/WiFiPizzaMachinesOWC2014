2a84d4d99e37fbdc693db42ec6536d062a34248b
Ellen Shapiro <designatednerd@gmail.com>
2014-04-12 21:45:24 -0500

    Update copyright text in footer.

--- wp-content/themes/fightlynch/footer.php
+++ wp-content/themes/fightlynch/footer.php
@@ -18,7 +18,7 @@
 						)); ?>
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> Colon Cancer Alliance for Research and Education for Lynch Syndrome</p>
 
 				</div>
 
