34d48c2a3d2243880bca06f44ec05604ac0c1976
colforbin13 <matt@irishguys.org>
2014-04-13 00:37:14 -0500

    no message

--- wp-content/themes/fightlynch/page-home.php
+++ wp-content/themes/fightlynch/page-home.php
@@ -27,50 +27,60 @@
 	  cssSlidy();
 	</script>
 
-			<div id="content" class="wrap">
+			<div id="content">
 
-				<div id="inner-content" class="cf">
+				<div id="inner-content" class="wrap cf">
 
-						<div id="main" class="m-all cf" role="main">
+						<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
 								<header class="article-header">
 
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
+									<h1 class="page-title"><?php the_title(); ?></h1>
 
 								</header>
 
-								<section class="entry-content cf">
-									
-									<?php the_content(); ?>
+								<section class="entry-content cf" itemprop="articleBody">
+									<?php
+										// the content (pretty self explanatory huh)
+										the_content();
+
+										/*
+										 * Link Pages is used in case you have posts that are set to break into
+										 * multiple pages. You can remove this if you don't plan on doing that.
+										 *
+										 * Also, breaking content up into multiple pages is a horrible experience,
+										 * so don't do it. While there are SOME edge cases where this is useful, it's
+										 * mostly used for people to get more ad views. It's up to you but if you want
+										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
+										 *
+										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
+										 *
+										*/
+										wp_link_pages( array(
+											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
+											'after'       => '</div>',
+											'link_before' => '<span>',
+											'link_after'  => '</span>',
+										) );
+									?>
 								</section>
 
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), _n( '<span>%</span> Comments', '<span>%</span> Comments', get_comments_number(), 'bonestheme' ) );?>
-									</p>
 
+								<footer class="article-footer">
 
-                 	<?php printf( __( '<p class="footer-category">Filed under: %1$s</p>', 'bonestheme' ), get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
+                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 
 								</footer>
 
+								<?php comments_template(); ?>
+
 							</article>
 
-							<?php endwhile; ?>
-
-									<?php bones_page_navi(); ?>
-
-							<?php else : ?>
+							<?php endwhile; else : ?>
 
 									<article id="post-not-found" class="hentry cf">
 											<header class="article-header">
@@ -80,16 +90,13 @@
 												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
 										</section>
 										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+												<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
 										</footer>
 									</article>
 
 							<?php endif; ?>
 
-
 						</div>
-
-					<?php // get_sidebar(); ?>
 
 				</div>
 
