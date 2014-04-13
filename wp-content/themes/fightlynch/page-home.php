da60bb011bbec6bd0f4002337d2bc2975f62ffb7
colforbin13 <matt@irishguys.org>
2014-04-12 23:33:16 -0500

    More header work, and template for home page to support carousel.

---
+++ wp-content/themes/fightlynch/page-home.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ Template Name: Home Page Template
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+<?php get_header(); ?>
+
+	<center><div id="slidy-container" class="wrap">
+	  <figure id="slidy">
+		<img src="images/dreamstime_5782247_DNA_cropped.jpg" />
+		<img src="images/dreamstime_7457010_scientist_cropped.jpg" />
+		<img src="images/dreamstime_8781320_group_cropped.jpg" />
+		<img src="images/dreamstime_13077435_doctors_cropped.jpg" />
+	  </figure>
+	</div></center>
+	<script type="text/javascript" src="js/cssslidy.js"></script>
+	<script type="text/javascript">
+	  cssSlidy();
+	</script>
+
+			<div id="content" class="wrap">
+
+				<div id="inner-content" class="cf">
+
+						<div id="main" class="m-all cf" role="main">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+
+								<header class="article-header">
+
+									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+									<p class="byline vcard">
+										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
+									</p>
+
+								</header>
+
+								<section class="entry-content cf">
+									
+									<?php the_content(); ?>
+								</section>
+
+								<footer class="article-footer cf">
+									<p class="footer-comment-count">
+										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), _n( '<span>%</span> Comments', '<span>%</span> Comments', get_comments_number(), 'bonestheme' ) );?>
+									</p>
+
+
+                 	<?php printf( __( '<p class="footer-category">Filed under: %1$s</p>', 'bonestheme' ), get_the_category_list(', ') ); ?>
+
+                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
+
+
+								</footer>
+
+							</article>
+
+							<?php endwhile; ?>
+
+									<?php bones_page_navi(); ?>
+
+							<?php else : ?>
+
+									<article id="post-not-found" class="hentry cf">
+											<header class="article-header">
+												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+										</header>
+											<section class="entry-content">
+												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+										</section>
+										<footer class="article-footer">
+												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+										</footer>
+									</article>
+
+							<?php endif; ?>
+
+
+						</div>
+
+					<?php // get_sidebar(); ?>
+
+				</div>
+
+			</div>
+
+
+<?php get_footer(); ?>
