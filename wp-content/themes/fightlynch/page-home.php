785bc1aeafa0570d3b2b92ea527dd1a994bad6d4
colforbin13 <matt@irishguys.org>
2014-04-21 10:39:45 -0500

    New social icons for contact page, added social links and disclaimer to footer.

--- wp-content/themes/fightlynch/page-home.php
+++ wp-content/themes/fightlynch/page-home.php
@@ -19,68 +19,54 @@
 				<div id="inner-content" class="wrap cf">
 
 						<div id="main" class="m-all cf" role="main">
+						
+							<?php if (have_posts()) : ?>
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+											 <!-- Dual content: Lynch Syndrome and Risks --><div style="width:100%;padding:0 7%;">
+							<?php  
+							$args = array('pages_per_post' => 2,'post_type' => 'page', 'order'=> 'ASC', 'orderby' => 'post_date' );
+							$postslist = the_excerpt( $args );
+							foreach ( $postslist as $post ) :
+							 setup_postdata( $post ); ?> 
+								<div style="float:left;width:270px;margin:0 43px;">
+									<?php the_title(); ?>   
+									<?php the_excerpt(); ?>
+									<?php ?>
+								</div>
+							<?php
+							endforeach; 
+							wp_reset_postdata();
+							?>
+							</div>
+											
+														
+								<hr>
+								 <!-- What type of person are you -->
+								 <!--  -->
+								 <!-- end. -->
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-								<header class="article-header">
-							
-
-								</header>
-
-								<section class="entry-content cf" itemprop="articleBody">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
-
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
-								</section>
-								
-
-
-								<footer class="article-footer">
-
-                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-								</footer>
-
-								<?php comments_template(); ?>
-
-							</article>
-
-							<?php endwhile; else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
+								 <!-- Activities/Events -->
+								 <h3>CCARE Activities</h3>    
+							<!-- do stuff to display 3 activities -->
+							<div style="width:100%;padding:0 7%;">
+							<?php  
+							$args = array( 'posts_per_page' => 3,'post_type' => 'event', 'order'=> 'ASC', 'orderby' => 'post_date' );
+							$postslist = get_posts( $args );
+							foreach ( $postslist as $post ) :
+							 setup_postdata( $post ); ?> 
+								<div style="float:left;width:270px;margin:0 13px;">
+									<?php if ( has_post_thumbnail() ) { ?> <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( array(270,270) ); ?></a> <?php } ?>
+									<div class="event_thumb_holder" style="height:18px;">&nbsp;</div>
+									<?php the_title(); ?>   
+									<div class="event_thumb_text_holder" style="height:10px;">&nbsp;</div>
+									<?php the_excerpt(); ?>
+									<?php ?>
+								</div>
+							<?php
+							endforeach; 
+							wp_reset_postdata();
+							?>
+							</div>
 							<?php endif; ?>
 
 						</div>
