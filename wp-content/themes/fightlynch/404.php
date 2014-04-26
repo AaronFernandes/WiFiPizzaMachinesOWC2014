526a472b02658b4f663e9ca7f84fbfc6fbe7612e
Ellen Shapiro <designatednerd@gmail.com>
2014-04-26 12:37:01 -0500

    Updating the 404 page.

--- wp-content/themes/fightlynch/404.php
+++ wp-content/themes/fightlynch/404.php
@@ -10,13 +10,13 @@
 
 							<header class="article-header">
 
-								<h1><?php _e( 'Epic 404 - Article Not Found', 'bonestheme' ); ?></h1>
+								<h1><?php _e( 'Page Not Found', 'bonestheme' ); ?></h1>
 
 							</header>
 
 							<section class="entry-content">
 
-								<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?></p>
+								<p><?php _e( 'We\'re sorry, the page you were looking for was not found. You can search here for more information:', 'bonestheme' ); ?></p>
 
 							</section>
 
@@ -27,8 +27,6 @@
 							</section>
 
 							<footer class="article-footer">
-
-									<p><?php _e( 'This is the 404.php template.', 'bonestheme' ); ?></p>
 
 							</footer>
 
