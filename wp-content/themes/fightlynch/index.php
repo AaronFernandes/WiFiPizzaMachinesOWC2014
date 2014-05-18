b4c488380a65d56c18ace18280b78fdd6300a340
Ellen Shapiro <designatednerd@gmail.com>
2014-05-18 03:47:06 -0500

    stray parentheseseses

--- wp-content/themes/fightlynch/index.php
+++ wp-content/themes/fightlynch/index.php
@@ -14,7 +14,7 @@
 
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 									<p class="byline vcard">
-										<?php printf( __( '<time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')) )); ?>
+										<?php printf( __( '<time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')) ); ?>
 									</p>
 
 								</header>
