c392c599c70ae8379e618efd6742549f08b6fccf
Ellen Shapiro <designatednerd@gmail.com>
2014-05-18 03:51:20 -0500

    Single title ensmallen

--- wp-content/themes/fightlynch/post-formats/format.php
+++ wp-content/themes/fightlynch/post-formats/format.php
@@ -18,7 +18,7 @@
 
                 <header class="article-header">
 
-                  <h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
+                  <h2 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h2>
 
                 </header> <?php // end article header ?>
 
